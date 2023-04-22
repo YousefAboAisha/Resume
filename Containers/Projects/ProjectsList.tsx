d12b84c8245efa4cb3dd2a96611043b1db4dc984
Yousef <yousef.aboesha@hotmail.com>
2023-04-22 14:06:32 +0200

    Fetch projects using SWR instead of regular fetch function

--- Containers/Projects/ProjectsList.tsx
+++ Containers/Projects/ProjectsList.tsx
@@ -4,81 +4,52 @@ import ProjectCard from "../../Components/UI/Cards/ProjectCard";
 import SkeletonLoading from "../../Components/UI/Utils/SkeletonLoading";
 import { Sites } from "../../Data/Sites";
 // import { storage } from "../../firebase";
+import useSWR from "swr";
+import { PUBLIC_URL } from "../../config";
 
 const Projects = () => {
-  // const storageRef = ref(storage, `/sites/`);
-  // const [files, setFiles] = useState([""]);
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  // const getFiles = async () => {
-  //   try {
-  //     const res = await listAll(storageRef);
-  //     const urls = await Promise.all(
-  //       res.items.map(async (item) => {
-  //         return await getDownloadURL(item);
-  //       })
-  //     );
-  //     console.log(urls);
-
-  //     setFiles(urls);
-  //   } catch (error) {
-  //     console.error("Error getting list of files:", error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getFiles();
-  // }, []);
-
-  const getData = () => {
-    setLoading(true);
-    fetch(
-      "https://resume-e8543-default-rtdb.europe-west1.firebasedatabase.app/projects/-NTIzBqbs0G8oQ-CapFj.json",
-      {
-        method: "GET",
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
+  const fetchProjects = async () => {
+    const res = await fetch(PUBLIC_URL);
+    return res.json();
   };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  const { data, error, isLoading } = useSWR(PUBLIC_URL, fetchProjects);
+
+  if (error)
+    return (
+      <div className="section w-full min-h-[400px]">
+        <p className="abs-center text-theme">Network Error!</p>
+      </div>
+    );
+  if (isLoading)
+    return (
+      <div className="section w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 min-h-[400px]">
+        <SkeletonLoading />
+        <SkeletonLoading />
+        <SkeletonLoading />
+        <SkeletonLoading />
+        <SkeletonLoading />
+        <SkeletonLoading />
+      </div>
+    );
+
+  console.log(data);
 
   return (
     <div className="section w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 min-h-[400px]">
-      {loading ? (
-        <>
-          <SkeletonLoading />
-          <SkeletonLoading />
-          <SkeletonLoading />
-        </>
-      ) : (
-        Sites.map((elem, index) => {
-          return (
-            <ProjectCard
-              key={index}
-              src={elem.href}
-              title={elem.title}
-              tags={elem.tags}
-              alt={elem.alt}
-              githubLink={elem.github_link}
-              liveLink={elem.live_link}
-            />
-          );
-        })
-      )}
+      {Sites.map((elem, index) => {
+        return (
+          <ProjectCard
+            key={index}
+            src={elem.href}
+            title={elem.title}
+            tags={elem.tags}
+            alt={elem.alt}
+            githubLink={elem.github_link}
+            liveLink={elem.live_link}
+          />
+        );
+      })}
     </div>
   );
 };
---
+++ config.ts
@@ -0,0 +1,2 @@
+export const PUBLIC_URL =
+  "https://resume-e8543-default-rtdb.europe-west1.firebasedatabase.app/projects/-NTIzBqbs0G8oQ-CapFj.json";
